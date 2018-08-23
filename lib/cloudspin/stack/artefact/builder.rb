6a4aba6d8bf3ffb74a46add4adc2628f11e8358a
kief <kmorris@thoughtworks.com>
2018-08-23 16:08:30 +0100

    Flexible way to add folders to be archived

--- lib/cloudspin/stack/artefact/builder.rb
+++ lib/cloudspin/stack/artefact/builder.rb
@@ -12,6 +12,7 @@ module Cloudspin
         attr_reader :stack_definition_name
         attr_reader :stack_definition_version
         attr_reader :dist_folder
+        attr_reader :folders_to_package
 
         def initialize(stack_definition:,
                        dist_folder:)
@@ -19,13 +20,24 @@ module Cloudspin
           @dist_folder = dist_folder
           @stack_definition_name = stack_definition.name
           @stack_definition_version = stack_definition.version
+          @folders_to_package = {
+            stack_definition.terraform_source_path => 'src'
+          }
         end
 
         def build
-          puts "Copying files to include in the artefact from #{stack_definition.terraform_source_path} to #{artefact_contents_folder}/src."
+          puts "Copying files to include in the artefact to #{artefact_contents_folder}/src."
           rm_rf(artefact_contents_folder)
-          mkpath File.dirname(artefact_contents_folder + '/src')
-          cp_r(stack_definition.terraform_source_path, artefact_contents_folder + '/src')
+          folders_to_package.each { |source, destination|
+            puts "Copying #{source} to #{artefact_contents_folder + '/' + destination}"
+            mkpath File.dirname(artefact_contents_folder + '/' + destination)
+            cp_r(source, artefact_contents_folder + '/' + destination)
+          }
+
+        end
+
+        def add_folder_to_package(source_folder, artefact_subfolder:)
+          @folders_to_package[source_folder] = artefact_subfolder
         end
 
         def package(version)
--- spec/cloudspin/stack/artefact/builder_spec.rb
+++ spec/cloudspin/stack/artefact/builder_spec.rb
@@ -9,13 +9,30 @@ RSpec.describe Cloudspin::Stack::Artefact::Builder do
     )
   }
 
-
   let(:builder) {  
     Cloudspin::Stack::Artefact::Builder.new(stack_definition: stack_definition, dist_folder: './dist')
   }
 
   it 'builds the expected artefact name' do
     expect(builder.artefact_name).to eq('example_stack-1.1.1')
+  end
+
+  it 'has only one folder by default' do
+    expect(builder.folders_to_package.size).to eq(1)
+  end
+
+  it 'includes source folder by default' do
+    expect(builder.folders_to_package).to include('some/path' => 'src')
+  end
+
+  it 'has two folders after one is added' do
+    builder.add_folder_to_package('./tests', artefact_subfolder: 'tests')
+    expect(builder.folders_to_package.size).to eq(2)
+  end
+
+  it 'includes added source folder' do
+    builder.add_folder_to_package('./tests', artefact_subfolder: 'tests')
+    expect(builder.folders_to_package).to include('./tests' => 'tests')
   end
 
 end
